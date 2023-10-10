75b9f699f3570f46d83417480f86cadc8bf2a91d
vlados1487 <vga.pan@mail.ru>
2023-10-10 22:21:27 +0300

    h

--- 2-if_else_switch/Change/change.h
+++ 2-if_else_switch/Change/change.h
@@ -1,3 +1,10 @@
 long long change(int a, int b, int c, int d){
-    return 0;
+    long long x1, x2;
+    x1 = 100 * a + b;
+    x2 = (long long)c * 100 + d;
+    if(x1 > x2 || a < 0 || c < 0 || b < 0 || d < 0) return -1;
+    
+    
+    
+    return x2 - x1;
 }
--- 2-if_else_switch/Chess/chess.h
+++ 2-if_else_switch/Chess/chess.h
@@ -1,3 +1,10 @@
 int chess_rook(char a, int b, char c, int d){
+    if(a < 'A' || a > 'H' || c < 'A' || c > 'H') return 0;
+    if(b < 1 || b > 8 || d < 1 || d > 8) return 0;
+    if(b == d && a != c || a == c && b != d) return 1;
+    
+    
+    
+    
     return 0;
 }
