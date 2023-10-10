f440fd30f170b733f67530c40b908ef3a4815e59
vlados1487 <vga.pan@mail.ru>
2023-10-11 01:43:06 +0300

    h

--- 2-if_else_switch/QuadraticEquation/quadratic_equation.h
+++ 2-if_else_switch/QuadraticEquation/quadratic_equation.h
@@ -1,3 +1,12 @@
 int QuadraticEquation(double a, double b, double c){
+    double d, x1, x2;
+    double sqrt(), pow();
+    
+    d = pow(b, 2) - 4 * a * c;
+    if (d >= 0){
+        x1 = (-b + sqrt(d, 2)) / 2 * a;
+        x2 = (-b - sqrt(d, 2)) / 2 * a;
+        return x1 + x2;
+        }
     return 0;
 }
