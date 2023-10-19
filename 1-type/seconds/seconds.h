d311640ff8ed6cf7f7d0c2888a77a422661fafea
vlados1487 <vga.pan@mail.ru>
2023-10-19 21:04:49 +0300

    h

--- 1-type/seconds/seconds.h
+++ 1-type/seconds/seconds.h
@@ -1,34 +1,80 @@
 int get_years(int seconds) {
-    return 0; // Not implementation
+    int i;
+    i = seconds / 32140800;
+    return 1970 + i;
+    
 }
 
 int get_month(int seconds) {
-    return 0; // Not implementation
+    int i;
+    if (seconds <= 32140800){
+    i = seconds / 2678400;
+    return 0 + i;
+    }if(seconds > 32140800){
+        int g;
+        g = (seconds / 32140800);
+        i = (seconds - g * 32140800) / 2678400;
+        return 0 + i;
+
+    } 
 }
 
 int get_day(int seconds) {
-    return 0; // Not implementation
+    int i, g;
+    if (seconds <= 2678400){
+        i = seconds / 86400;
+        return 0 + i;
+    }if (seconds > 2678400){
+        g = seconds / 2678400;
+        i = (seconds - g * 2678400) / 86400;
+        return 0 + i;
+    }
 }
 
 int get_hours(int seconds) {
-    return 0; // Not implementation
+    int i, g;
+    if (seconds <= 86400){
+        i = seconds / 3600;
+        return 0 + i;
+        }if (seconds > 86400){
+            g = seconds / 86400;
+            i = (seconds - g * 86400) / 3600;
+            return 0 + i;
+        }
 }
 
 int get_minutes(int seconds) {
-    return 0; // Not implementation
+    int i, g;
+    if (seconds < 3600){
+        i = seconds / 60;
+        return 0 + i;
+    }if(seconds > 3600){
+        g = seconds / 3600;
+        i = (seconds - g * 3600) / 60;
+    return 0 + i;
+    }
 }
 
 int get_seconds(int seconds) {
-    return 0; // Not implementation
+    int i, g;
+    if (seconds < 60){
+        return 0 + seconds;
+    }if(seconds > 60){
+        g = seconds / 60;
+        i = (seconds - g * 60);
+    }
+    return 0 + i;
 }
 
 
 int get_magical_value(int seconds) {
+    int g;
     int year = get_years(seconds);
     int month = get_month(seconds);
     int day = get_day(seconds);
     int hours = get_hours(seconds);
     int minutes = get_minutes(seconds);
     int second = get_seconds(seconds);
-    return 0; // Not implementation
+    g = 1 + day + seconds;
+    return year + month * pow(g, 2) ; // Not implementation
 }
