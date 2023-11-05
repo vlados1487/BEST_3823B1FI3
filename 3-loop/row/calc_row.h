27cfc67a84a0b16f7ed747118dec3afdd62009b7
vlados1487 <vga.pan@mail.ru>
2023-11-06 01:15:45 +0300

    k

--- 3-loop/row/calc_row.h
+++ 3-loop/row/calc_row.h
@@ -1,3 +1,10 @@
 long long calc_row(int n, long long k){
-    return 0;
+    int a = 1;
+    long long o = 0;
+    while (a <= n){
+        o += k * a;
+        a++;
+    }if(k < 0 && o > 0){
+        return 0;
+    }else return o;
 }
