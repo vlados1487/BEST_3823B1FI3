29ee3de682a27a90feb2637d2573c63bed354d2a
vlados1487 <vga.pan@mail.ru>
2023-11-05 23:28:23 +0300

    k

--- 3-loop/find_digit/find_digit.h
+++ 3-loop/find_digit/find_digit.h
@@ -1,20 +1,52 @@
 #include <math.h>
 
 char find_digit(double number, char digit){
-    long celoe[6], ostatok[11], i = 0, m = number;
-    if(number > 1){
-    while(m > 10){
-        m = number / 10;
-        break;
-        
+    long double p;
+    double L, y;
+    int a[20], i = 0, o, j, cnt = 0, u;
+    y = modf(number, &L);
+    u = (int)number;
+    p = number - u;
+    o = p * 1000000000;
+    if(digit > 9 || digit < 0) return -1;
+    if(y > 0){
+    for(i = 0; o > 0; i++)
+    {
+        a[i] = o % 10;
+        o /= 10;
+        }
+    for(j = i + 1; u > 0; j++){
+        a[j] = u % 10;
+        u /= 10;
+    }
+    i = j;
+    for(j = j - 1; j >= 0; j--){
+        if(a[j] != digit){
+            cnt++;
+        }
+        if(a[j] == digit) break;
 
-        
 
+    }cnt += 1;
+    return cnt;
+    }
+    if(y == 0){
+        long long u = number;
+        cnt = 1;
+    for(j = 0; u > 0; j++){
+        a[j] = u % 10;
+        u /= 10;
+    }
+    for(j = j - 1; j >= 0; j--){
+        if(a[j] != digit){
+            cnt++;
+        }
+        if(a[j] == digit) break;
 
 
     }
+    return cnt;
     }
     
-    
-    return 100;
+
 }
--- 3-loop/find_digit/test.c
+++ 3-loop/find_digit/test.c
@@ -8,7 +8,7 @@ void test_find_digit() {
     TEST_ASSERT(find_digit(1.23457880, 3) == 4);
     TEST_ASSERT(find_digit(9.99111, 1) == 5);
     TEST_ASSERT(find_digit(18335, -1) == -1);
-    TEST_ASSERT(find_digit(11335593925544, 2) == 10);
+    TEST_ASSERT(find_digit(11353593925544, 2) == 10);
 }
 
 TEST_LIST = {
