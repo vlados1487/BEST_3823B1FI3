428ecd8bf596d21e64d313ec2a96dc803eeadd3e
vlados1487 <vga.pan@mail.ru>
2023-11-05 20:45:01 +0300

    k

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -2,7 +2,29 @@
 
 int find_elem(int left, int right, int number, int max_iter){
     int i = 0; //Счётчик итераций
+    int cnt = 1, u, k = 0, l, r;
+    l = left;
+    r = right;
+    while(k != number){
+        
+        k = (r + l) / 2;
+        if (k > number){
+            r = k - 1;
+            
+
+        }if (k < number){
+            l = k + 1;
+            
+        }if(k == number){
+            break;
+        }
+        cnt++;
+        
+    }if (left == 1) u = cnt * (number - 1);
+    if (left == 0) u = cnt * number;
+    return u;
 
 
     return i; //НЕ МЕНЯТЬ!
+
 }
