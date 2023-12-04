81de415bb60b917f2bd48ca590adcbdcc518682b
vlados1487 <vga.pan@mail.ru>
2023-12-04 19:42:24 +0300

    k

--- 3-loop/integral/integral.h
+++ 3-loop/integral/integral.h
@@ -1,5 +1,10 @@
 #include <math.h>
-
+#include <stdio.h>
 double integral(double a, double b, double c, double left, double right){
-    return 0.0;
+    double l, r;
+    r = right - left;
+    l = (a * pow(r, 3)) / 3 + (b * pow(r, 2)) / 2 + c * r;
+    if(l >= 0) return l;
+    if(l < 0) return -1;
+
 }
