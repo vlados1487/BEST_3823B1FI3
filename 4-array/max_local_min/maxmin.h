5a4b5a74f28ac786730ad950f70a27f8c474c800
vlados1487 <vga.pan@mail.ru>
2023-12-07 02:11:14 +0300

    k

--- 4-array/max_local_min/maxmin.h
+++ 4-array/max_local_min/maxmin.h
@@ -1,3 +1,19 @@
 int maxmin(int input[], int size){
-    return -1;
+    int min = -100;
+    for(int i = 0; i < size; i++){
+        if(i == 0){
+            if(input[i + 1] > input[i] && min < input[i]){
+                min = input[i];
+            }
+        }
+        if(input[i - 1] > input[i] && input[i + 1] > input[i] && (min < input[i])){
+            min = input[i];
+            }
+        if(i == size - 1){
+            if(input[i - 1] > input[i] && min < input[i]){
+            min = input[i];
+            }
+        }    
+        }
+        return min;
 }
--- 4-array/more_on_left/left_more.h
+++ 4-array/more_on_left/left_more.h
@@ -1,3 +1,9 @@
 char left_more(double input[], unsigned int size){
-    return -1;
+    int cnt = 0;
+    for(int i = size - 1; i > 0; i--){
+        if(input[i] > input[i - 1]){
+            cnt++;
+        }
+    }
+    return cnt;
 }
