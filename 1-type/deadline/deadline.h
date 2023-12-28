88becf82c746e1d4c40763fdf8b8000c6a153b71
vlados1487 <vga.pan@mail.ru>
2023-12-29 01:27:59 +0300

    k

--- 1-type/deadline/deadline.h
+++ 1-type/deadline/deadline.h
@@ -4,7 +4,7 @@ int get_score(short score, int start, int stop, int now) {
     r = stop - start;
     m = (score / 2) / r;
         if (now > stop){
-            return score / 2;
+            return (score + 1) / 2;
         }if (now  <= start){
             return score;
         }if (now >= start && now <= stop){
--- 5-pointer/packaging/pack.h
+++ 5-pointer/packaging/pack.h
@@ -1,13 +1,17 @@
 #include <math.h>
 
 unsigned long long pack_ull(unsigned char arr[], int size){
-   if(sizeof(arr) > 8) return 0;
+   if(size > 8) return 0;
    unsigned long long l = 0;
-   for (int i = 0; i < size; ++i) {
-    l = l | ((unsigned long long)arr[i] << (size * i));
-  }
+   for (int i = 0; i < size; i++) {
+    l = l + ((unsigned long long)arr[i] << (8 * i));
+  }return l;
 }
 
 unsigned char unpack_ull(unsigned long long input){
-    return 5;
+    int putin = 4;
+    unsigned char putout = (input >> 8 * putin)&0xff;
+    return putout;
+    
+     
 }
