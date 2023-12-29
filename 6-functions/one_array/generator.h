25b78ede743259183b1883c371363ad2f443ec26
vlados1487 <vga.pan@mail.ru>
2023-12-29 04:34:08 +0300

    k

--- 6-functions/one_array/generator.h
+++ 6-functions/one_array/generator.h
@@ -1,5 +1,35 @@
 #include <math.h>
-
+#include <stdlib.h>
 int* array_generator(int* real_size, int k, int m, int f){
-    return 0;
+    *real_size = 12;
+    int* a = (int*)malloc(*real_size * sizeof(int));
+    a[0] = 0;
+    a[*real_size - 1] = f;
+    
+    if (f  >= m && f  <= k){
+        for(int i = 1; i < *real_size - 1; i++){
+            a[i] = 1;
+        }
+        
+
+    }
+    if(f < m){
+        for(int j = 2; j < *real_size - 1; j++){
+                a[j] = 1;
+            }
+        while(a[1] < m){
+            a[1] += 1;
+            }
+        if(a[1] > f) {
+            a[1] -= (f - 1);
+            a[2] += (f - 1);
+        }
+
+    }
+    if(f == 0){
+        for(int j = 0; j < *real_size; j++){
+            a[j] = 0;
+        }
+    }
+    return a;
 }
